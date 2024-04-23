bbec599fc7a8c4018458fd84d1ca9f4c0b426168
KVBAMusic <kubaokon8@gmail.com>
2024-04-23 09:16:50 +0200

    set up login page for backend integration

--- src/login.js
+++ src/login.js
@@ -1,13 +1,36 @@
 function login(userText, passText) {
-    var user = document.getElementById(userText).value;
-    var pass = document.getElementById(passText).value;
-    var outElement = document.getElementById('login-pass-output');
+    var status
+    var user = document.getElementById(userText).value
+    var pass = document.getElementById(passText).value
+    var outElement = document.getElementById('login-pass-output')
     if (user.length === 0 || pass.length === 0) {
-        outElement.innerHTML = "Invalid credentials";
-        return false;
+        outElement.innerHTML = "Invalid credentials"
+        return
     }
-    // tutaj daj request do backendu żeby zweryfikował
-    outElement.innerHTML = "";
-    window.open("index.html", "_self");
-    return true;
+    outElement.innerHTML = ""
+    // TODO: dej tu port
+    fetch("localhost/login:PORT", {
+        method: "POST",
+        body: {
+            username: user,
+            password: pass
+        }
+    })
+    .then((response) => {
+        if (typeof response === 'object' && response !== null) {
+            return response
+        }
+        return response.json()
+    })
+    .then((msg) => {
+        status = msg.status
+        console.log(msg.status)
+    })
+    if (statusOk(status)) {
+        //window.open("index.html", "_self");
+    }
+}
+
+function statusOk(status) {
+    return status >= 200 && status < 300
 }
