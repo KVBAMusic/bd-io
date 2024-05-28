319ef6902f22742e4c3988b2a667821e5f29bc0a
KVBAMusic <kubaokon8@gmail.com>
2024-05-28 21:16:09 +0200

    updated colours, started course page

--- src/login.js
+++ src/login.js
@@ -8,27 +8,28 @@ function login(userText, passText) {
         return
     }
     outElement.innerHTML = ""
+    window.open("course.html", "_self")
     // TODO: dej tu port
-    fetch("localhost/login:PORT", {
-        method: "POST",
-        body: {
-            username: user,
-            password: pass
-        }
-    })
-    .then((response) => {
-        if (typeof response === 'object' && response !== null) {
-            return response
-        }
-        return response.json()
-    })
-    .then((msg) => {
-        status = msg.status
-        console.log(msg.status)
-    })
-    if (statusOk(status)) {
-        //window.open("index.html", "_self");
-    }
+    // fetch("localhost/login:PORT", {
+    //     method: "POST",
+    //     body: {
+    //         username: user,
+    //         password: pass
+    //     }
+    // })
+    // .then((response) => {
+    //     if (typeof response === 'object' && response !== null) {
+    //         return response
+    //     }
+    //     return response.json()
+    // })
+    // .then((msg) => {
+    //     status = msg.status
+    //     console.log(msg.status)
+    // })
+    // if (statusOk(status)) {
+    //     //window.open("index.html", "_self");
+    // }
 }
 
 function statusOk(status) {
